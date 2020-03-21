4b68cad62e7daa972751c0e7a0e1b875afa2028d
penpeng <cxyy521514@foxamil.com>
2020-03-21 10:30:52 +0800

    update

--- src/services/dept.ts
+++ src/services/dept.ts
@@ -1,7 +1,5 @@
 import { get, post } from '@/utils/request';
-import { stringify } from 'qs';
 export function query<T>(params: any): Promise<T> {
-  console.log(params);
   return get<T>('/api/dept/list/deptTree', params);
 }
 export function modify<T>(params: any): Promise<T> {
@@ -20,7 +18,6 @@ export function removeUser<T>(ids: any[]): Promise<T> {
   return get<T>(`/api/user/dept/bulkDelete?ids=${ids.join(',')}`);
 }
 export function addusers<T>({ userIds, targetDeptIds }: any): Promise<T> {
-  console.log(userIds, targetDeptIds);
   return get<T>(
     `/api/user/dept/bulkAddAssociation?userIds=${userIds.join(
       ',',
