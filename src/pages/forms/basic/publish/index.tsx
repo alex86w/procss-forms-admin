710a4a4e3acd7386a7b95189427c99ff0a3cf504
pollerhame <36783131@qq.com>
2020-03-25 17:20:49 +0800

    表单发布页面

--- src/pages/forms/basic/publish/index.tsx
+++ src/pages/forms/basic/publish/index.tsx
@@ -1,21 +1,151 @@
 import React, { } from 'react';
-import { Col, Row } from 'antd';
+import { PageHeader, Divider, Button, Modal, Switch, Typography, Form, Input, Row, Col, notification } from 'antd';
+import { PlusOutlined, DeleteOutlined } from '@ant-design/icons';
+import styles from './style.less';
 
-const Publish = function () {
-
-
-    return <Row style={{ width: "100%" }}>
-        <Col span={6} />
-        <Col span={12}>
-            <div>
-                表单发布
-            </div>
-            <div>
-
-            </div>
-
-        </Col>
-        <Col span={6} />
-    </Row>
+const itemLayout = {
+    labelCol: { span: 18 },
+    wrapperCol: { span: 6 },
 }
+
+class Publish extends React.Component {
+    state = { visible: false, display: 'none', extvisible: false, extDiv: 'none', pwdDiv: 'none', userVisible: false }
+    handleOk = () => {
+        this.setState({ visible: false, userVisible: true })
+    }
+    handleCancle = () => {
+        this.setState({ visible: false })
+    }
+    handleShowModal = () => {
+        this.setState({ visible: true })
+    }
+    handleSwitch = (e) => {
+        if (e) {
+            this.setState({ display: 'block' })
+        }
+        if (!e) {
+            this.setState({ display: 'none' })
+        }
+    }
+    handleSetExt = (e) => {
+        if (e) {
+            this.setState({ extDiv: 'block' })
+        }
+        if (!e) {
+            this.setState({ extDiv: 'none' })
+        }
+    }
+    handlePwd = (e) => {
+        if (e) {
+            this.setState({ pwdDiv: 'block' })
+        }
+        if (!e) {
+            this.setState({ pwdDiv: 'none' })
+        }
+    }
+    addUrlQuery = () => {
+        const val = document.querySelector('#urlQuery');
+        if (val.value.trim() === '') {
+            notification.error({ message: '扩展字段不能为空' })
+        }
+    }
+    handleSubmit = () => {
+        const val = document.querySelector('#pwd');
+        if (val.value.trim() === '') {
+            notification.error({ message: '密码不能为空' })
+        }
+    }
+    render() {
+        return (
+            <div className={styles.containor}>
+                <div className={styles.gpline}><span className={styles.title}>团队成员</span><span className={styles.content}>将表单发布给团队成员，成员登录系统后可填写表单</span></div>
+                <div className={styles.body}>
+                    <Button type='link' style={{ marginTop: 5 }} icon={<PlusOutlined />} onClick={this.handleShowModal}>点击选择成员</Button>
+                </div>
+                <div>
+                    <div className={styles.gpline}><span className={styles.title}>公开链接</span><span className={styles.content}>将表单发布为公开链接，无需登录即可填写表单</span><span className={styles.link}><a>《外链管理规范》</a></span></div>
+                    <div className={styles.gpline} style={{ padding: '10px 24px' }}>
+                        <Switch checkedChildren="开" unCheckedChildren="关" onChange={this.handleSwitch} />
+                    </div>
+                    <div className={styles.gpline} style={{ display: this.state.display }}>
+                        <div style={{ paddingTop: 15, fontSize: 16 }}>链接地址</div>
+                        <Row>
+                            <Col><Typography.Paragraph copyable strong>https://t56wl49c7o.jiandaoyun.com/f/5e60a671b7354c0006f544c7</Typography.Paragraph></Col>
+                            <Col><Button style={{ marginLeft: 10 }} onClick={() => window.open('https://t56wl49c7o.jiandaoyun.com/f/5e60a671b7354c0006f544c7')}>打开</Button><Button style={{ marginLeft: 10 }} onClick={() => this.setState({ extvisible: true })}>外联扩展</Button></Col>
+                        </Row>
+                    </div>
+                    <div className={styles.gpline}>
+                        <div style={{ paddingTop: 15, fontSize: 16 }}>填写设置</div>
+                        <Row align='middle' gutter={16} style={{ marginBottom: 20 }}>
+                            <Col>凭密码填写</Col>
+                            <Col><Switch checkedChildren="开" unCheckedChildren="关" onChange={this.handlePwd} /></Col>
+                            <Col style={{ display: this.state.pwdDiv }}>
+                                <Input style={{ width: 200 }} id='pwd' /><Button style={{ marginLeft: 10 }} type='primary' onClick={this.handleSubmit}>保存密码</Button>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+                <Modal
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancle}
+                    okText='邀请成员'
+                    cancelText='关闭'
+                >
+                    <div style={{ fontSize: 16, marginTop: 40, lineHeight: '40px' }}>团队中还没有成员，现在开始邀请成员吧</div>
+                    <div style={{ fontSize: 12, lineHeight: '18px', color: '#5E6D82', marginBottom: 20 }}>受到邀请用户加入你的团队后，你可以发布表单给他/她填写。</div>
+                </Modal>
+                <Modal
+                    title='外联扩展'
+                    visible={this.state.extvisible}
+                    onOk={() => this.setState({ extvisible: false })}
+                    onCancel={() => this.setState({ extvisible: false })}
+                    okText='提交'
+                    cancelText='关闭'
+                    width='700px'
+                >
+                    <div style={{ fontSize: 12, lineHeight: '18px', color: '#5E6D82', marginBottom: 20 }}>你可以给链接添加扩展属性值，从而生成多个链接地址。这些地址可以发布到不同的渠道中，用来区分数据来源</div>
+                    <Switch checkedChildren="开" unCheckedChildren="关" onChange={this.handleSetExt} />
+                    <div style={{ display: this.state.extDiv }}>
+                        <Row style={{ marginTop: 20, backgroundColor: '#F3F6FC', padding: '10px 0px', marginBottom: 20 }} gutter={16} align='middle'>
+                            <Col span={12}>https://t56wl49c7o.jiandaoyun.com/f/5e60a671b7354c0006f544c7</Col>
+                            <Col span={12}> <Input style={{ width: '70%' }} id='urlQuery' /><Button style={{ marginLeft: 10 }} onClick={this.addUrlQuery}>添加</Button></Col>
+                        </Row>
+                        <div>
+                            <Row>
+                                <Col span={6}>拓展字段</Col>
+                                <Col span={18}>链接</Col>
+                            </Row>
+                            <Row align='middle' style={{ backgroundColor: '#F3F6FC', padding: '10px 0px', marginTop: 5 }}>
+                                <Col span={6}>111111</Col>
+                                <Col span={16}>https://t56wl49c7o.jiandaoyun.com/f/5e60a671b7354c0006f544c7?111111</Col>
+                                <Col span={2}>
+                                    <Button icon={<DeleteOutlined />} />
+                                </Col>
+                            </Row>
+                            <Row align='middle' style={{ backgroundColor: '#F3F6FC', padding: '10px 0px', marginTop: 5 }}>
+                                <Col span={6}>111111</Col>
+                                <Col span={16}>https://t56wl49c7o.jiandaoyun.com/f/5e60a671b7354c0006f544c7?111111</Col>
+                                <Col span={2}>
+                                    <Button icon={<DeleteOutlined />} />
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                </Modal>
+                <Modal
+                    visible={this.state.userVisible}
+                    onOk={() => this.setState({ userVisible: false })}
+                    onCancel={() => this.setState({ userVisible: false })}
+                    okText='邀请'
+                    cancelText='取消'
+                    title='邀请成员'
+                >
+                    成员Form
+                </Modal>
+            </div >
+        )
+    }
+}
+
 export default Publish
