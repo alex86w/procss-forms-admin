f9e59fb307d03ab4710055278a79c5605d963e7e
penpeng <cxyy521514@foxamil.com>
2020-03-25 20:46:26 +0800

    update fieldsCheckBox

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -19,18 +19,48 @@ export interface CheckProps {
   model: any;
   keyr: string;
 }
+const handleChange = function ({ model, keyr, value, onChange }: any) {
+  const editable = (model[keyr] || []).filter((it: string) => it.includes('editable'));
+  const visible = (model[keyr] || []).filter((it: string) => it.includes('visible'));
+  const brief = (model[keyr] || []).filter((it: string) => it.includes('brief'));
+  let _editable: string[] = value.filter((it: string) => it.includes('editable'));
+  const _visible: string[] = value.filter((it: string) => it.includes('visible'));
+  let _brief: string[] = value.filter((it: string) => it.includes('brief'));
+  const editableAdd = _editable.filter(it => !editable.includes(it));
+  const visibleRemove: string[] = visible.filter((it: string) => !_visible.includes(it));
+  if (editableAdd.length > 0) {
+    editableAdd.forEach(it => _visible.push(it.split(':')[0] + ':visible'))
+  }
+  if (visibleRemove.length > 0) {
+    visibleRemove.forEach(item => {
+      _editable = _editable.filter(it => !it.includes(item.split(':')[0]))
+    })
+  }
+  if (brief.length === 3 && _brief.length > 3) {
+    _brief = brief as string[];
+  }
+  const _value = [..._brief, ..._visible, ..._editable]
+
+  onChange && onChange(_value)
+
+
+
+}
+
 
 
 export const CustomCheckBox = function (props: CheckProps) {
   const { onChange, title, style, data, model, keyr } = props;
   const len = title.length;
   const span = 24 / (len + 1);
+
+
   return (
     <div style={style}>
       <div className={styles.headerbar}>字段权限控制</div>
       <Group
         style={{ width: '100%' }}
-        onChange={v => onChange(v)}
+        onChange={value => handleChange({ model, keyr, value, onChange })}
         value={model[keyr] || []}
       >
         <Row>
