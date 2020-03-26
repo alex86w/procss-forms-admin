0e8119e85ce138d02f4bf92a14bb30d193b5b80c
penpeng <cxyy521514@foxamil.com>
2020-03-26 20:20:53 +0800

    update

--- src/pages/mobile/TodoDetail/index.tsx
+++ src/pages/mobile/TodoDetail/index.tsx
@@ -1,23 +1,24 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 import Form from '..';
 import { Button } from 'antd';
 import styles from './index.less';
-import AwesomeModal from '../components/AwesomeModal';
+import AwesomeModal, { VisiType } from '../components/AwesomeModal';
 
 
 const ToDoDetail = function (props: any) {
+    const [visitype, $visitype] = useState<VisiType>('none')
 
     return <div style={{ width: "100%" }}>
         <div style={{ width: "100%", paddingBottom: 130, background: 'transparent' }}>
             <Form />
         </div>
         <div className={styles.nav}>
-            <Button className={styles.button} type="primary"><div>审批意见</div></Button>
-            <Button className={styles.button} type="primary"><div>流程日志</div></Button>
-            <Button className={styles.button} type="primary"><div>评论</div></Button>
+            <Button className={styles.button} type="primary" onClick={() => $visitype('suggest')}><div>审批意见</div></Button>
+            <Button className={styles.button} type="primary" onClick={() => $visitype('flowLog')}><div>流程日志</div></Button>
+            <Button className={styles.button} type="primary" onClick={() => $visitype('comment')}><div>评论</div></Button>
             <Button className={styles.button} type="primary"><div>流转图</div></Button>
         </div>
-        <AwesomeModal/>
+        <AwesomeModal visitype={visitype} $visitype={$visitype} />
     </div>
 
 
--- src/pages/mobile/components/AwesomeModal.tsx
+++ src/pages/mobile/components/AwesomeModal.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Modal, List, } from 'antd-mobile';
 import styles from './styles.less';
 import { Button } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { BellTwoTone } from '@ant-design/icons';
 
 export type VisiType = 'suggest' | 'comment' | 'flowLog' | 'none';
 
@@ -13,27 +13,27 @@ for (let i = 0; i < 10; i++) {
 const suggest = (item: any) => (
     <div className={styles.suggest}>
         <div className='item-header'>流程发起节点 </div>
-        <div className="space-between"><span><UserOutlined /> jdy123123123</span><span>时间</span> </div>
+        <div className="space-between"><span><BellTwoTone twoToneColor="#eb2f96" /> jdy123123123</span><span /> </div>
         <div className="content"><span>123</span></div>
         <div className='time'>
-            <div><span>开始时间：2020</span> </div>
-            <div><span>结束时间：2020</span></div>
+            <div><span>开始时间：2020-01-19 18:34:15</span> </div>
+            <div><span>结束时间：2020-01-19 18:34:15</span></div>
         </div>
     </div>
 )
 const comment = (item: any) => (
     <div className={styles.comment}>
-        <div className="space-between"><span><UserOutlined /> jdy123123123</span><span>时间</span> </div>
+        <div className="space-between"><span><BellTwoTone twoToneColor="#eb2f96" /> jdy123123123</span><span>时间</span> </div>
         <div className="content"><span>123</span></div>
     </div>
 )
 const flowLog = (item: any) => (
     <div className={styles.flowLog}>
         <div className='item-header'>流程发起节点 </div>
-        <div className='space-between'><span><UserOutlined />jdy123123123</span><span>提交</span> </div>
+        <div className='space-between'><span><BellTwoTone twoToneColor="#eb2f96" />jdy123123123</span><span>提交</span> </div>
         <div className='time'>
-            <div><span>开始时间：2020</span> </div>
-            <div><span>结束时间：2020</span></div>
+            <div><span>开始时间：2020-01-19 18:34:15</span> </div>
+            <div><span>结束时间：2020-01-19 18:34:15</span></div>
         </div>
     </div>
 )
@@ -41,7 +41,7 @@ const flowLog = (item: any) => (
 
 const renderFooter = (active: boolean, $active: React.Dispatch<React.SetStateAction<boolean>>) => (
     <div className={styles.footer}>
-        {!active && <input style={{ width: "88%", marginBottom: 10, border: '1px solid rgba(0,0,0,.3)' }} onFocus={() => $active(true)} />}
+        {!active && <input className={styles.trigger} onFocus={() => $active(true)} placeholder="请填写评论意见...." />}
         {active && <div style={{ width: "100%" }}>
             <textarea style={{ width: "88%", height: "100px" }} autoFocus />
             <Button type="primary" style={{ width: '100%' }}>提交</Button>
@@ -49,13 +49,20 @@ const renderFooter = (active: boolean, $active: React.Dispatch<React.SetStateAct
     </div>
 )
 interface AwesomeModalProps {
-    visitype: VisiType
+    visitype: VisiType;
+    $visitype: (v: VisiType) => void
+}
+
+const renders = {
+    comment,
+    flowLog,
+    suggest
 }
 
 
 const AwesomeModal = function (props: AwesomeModalProps) {
     const [active, $active] = useState(false)
-    const { visitype } = props;
+    const { visitype, $visitype } = props;
 
     const title: { [key: string]: string } = {
         comment: '评论',
@@ -66,15 +73,20 @@ const AwesomeModal = function (props: AwesomeModalProps) {
     return (
         <Modal
             popup
-            visible={true}
+            visible={visitype !== 'none'}
             animationType="slide-up"
             title={title[visitype]}
+            onClose={() => {
+                $visitype('none');
+                if (active) $active(false);
+            }}
         >
             <List
                 style={{ height: '55vh', overflow: 'scroll' }}
             >
                 {data.map(it => (<List.Item key={it}>
-                    {suggest(it)}
+                    {visitype !== 'none' && renders[visitype](it)}
+
                 </List.Item>))}
                 <List.Item style={{ height: 50, textAlign: 'center' }}>
                     <div style={{ width: "100%", padding: "15px 8px", fontSize: 13, color: "rgba(0,0,0,.3)", height: 50 }}> 没有更多...</div>
