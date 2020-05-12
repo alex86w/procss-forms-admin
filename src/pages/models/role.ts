c913b82694518541d9cd70b906db2544c1700cba
penpeng <cxyy521514@foxamil.com>
2020-05-12 16:17:34 +0800

    update

---
+++ src/pages/models/role.ts
@@ -0,0 +1,91 @@
+import { notification } from 'antd';
+import { Model } from 'dva';
+import { AnyAction } from 'redux';
+import * as api from '@/services/role'
+
+export default {
+    namespace: 'role',
+    state: {
+        data: [],
+        queryParams: {
+            total: 0,
+            page: 0,
+            size: 10
+        }
+    },
+    reducers: {
+        changeState(state, { payload }: AnyAction) {
+            return { ...state, ...payload }
+        }
+    },
+    effects: {
+        *query({ payload }, { call, put, select }) {
+            let queryParams = yield select((state: any) => state.role.queryParams);
+            queryParams = { ...queryParams, ...payload };
+            const res = yield call(api.query, queryParams);
+            if (res.success) {
+                yield put({
+                    type: 'changeState',
+                    payload: {
+                        data: res.data,
+                        queryParams: { ...queryParams, total: res.count }
+                    }
+                })
+            } else {
+                notification.error({ message: JSON.stringify(res.message) })
+            }
+        },
+        *modify({ payload, callback }, { call, put }) {
+            const res = yield call(api.modify, payload);
+            if (res.success) {
+                notification.success({ message: '操作成功' })
+                callback && callback(true)
+                yield put({
+                    type: 'changeState'
+                })
+            } else {
+                notification.error({ message: res.message || '操作失败' })
+            }
+        },
+        *create({ payload, callback }, { call, put }) {
+            const res = yield call(api.create, payload);
+            if (res.success) {
+                notification.success({ message: '操作成功' })
+                callback && callback(true)
+                yield put({
+                    type: 'changeState'
+                })
+            } else {
+                notification.error({ message: res.message || '操作失败' })
+            }
+        },
+        *remove({ payload, callback }, { call, put }) {
+            const res = yield call(api.remove, payload);
+            if (res.success) {
+                notification.success({ message: '操作成功' });
+                callback && callback(true)
+                yield put({
+                    type: 'changeState'
+                })
+            } else {
+                notification.error({ message: res.message || '操作失败' })
+            }
+        }
+    },
+    subscriptions: {
+        init({ history, dispatch }) {
+            history.listen(({ pathname }) => {
+                if (pathname === '/system/role') {
+                    let user: any = localStorage.getItem('user');
+                    user = user ? JSON.parse(user) : {};
+                    dispatch({
+                        type: 'query',
+                        payload: {
+                            rootDeptId: user.rootDeptId
+                        }
+                    })
+                }
+            })
+        }
+    }
+} as Model
--- src/pages/system/role.tsx
+++ src/pages/system/role.tsx
@@ -1,31 +1,55 @@
 import * as React from 'react';
 import { Table, Button } from 'antd';
 import { ColumnType } from 'antd/lib/table';
+import { connect } from 'umi';
 interface RoleProps {
-
+  data: any[];
+  queryParams: {
+    page: number,
+    size: number,
+    total: number,
+  }
+  dispatch: (v: any) => void
 }
 
 const Role = function (props: RoleProps) {
   const columns = [
     { title: '角色名', dataIndex: 'name', key: 'name' },
     { title: '描述', dataIndex: 'description', key: 'description' },
-    { title:'操作',key:'operation',render:_=>{
-      return (
-      <>
-      <Button>修改</Button>
+    {
+      title: '操作', key: 'operation', render: _ => {
+        return (
+          <>
+            <Button>修改</Button>
       &nbsp;&nbsp;
-      <Button>删除</Button>
-      </>
-      )
-    }},
+            <Button>删除</Button>
+          </>
+        )
+      },
+      align: 'right'
+    },
   ] as ColumnType<any>[];
 
   return (
     <div>
-      <Button>新建</Button>
+      <Button style={{ marginBottom: 20 }}>新建</Button>
       <Table
         columns={columns}
         rowKey="id"
+        dataSource={props.data}
+        pagination={{
+          current: props.queryParams.page,
+          pageSize: props.queryParams.size,
+          total: props.queryParams.total,
+          onChange: v => props.dispatch({
+            type: 'role.query',
+            payload: {
+              page: v - 1
+            }
+          })
+
+        }}
+
       />
     </div>
   );
@@ -34,6 +58,12 @@ const Role = function (props: RoleProps) {
 Role.title = '角色管理';
 Role.icon = 'safetyCertificate';
 Role.sort = 5;
-Role.access= [] as string[]
+Role.access = [] as string[]
 
-export default Role;
+const ConnectedRole = connect(({ role, loading }: any) => ({
+  data: role.data,
+  queryParams: role.queryParams,
+  loading: loading.models.role
+}))(Role);
+
+export default ConnectedRole;
