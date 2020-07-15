c9e71dc54d43292d7088257df21bed714d1f6664
pollerhame <36783131@qq.com>
2020-07-15 17:13:34 +0800

    update

--- src/pages/mobile/login/index.tsx
+++ src/pages/mobile/login/index.tsx
@@ -21,7 +21,7 @@ class Login extends Component<{ dispatch: Dispatch<any> }> {
             <div className={styles.containor}>
                 <div className={styles.topContainor}>
                     <div className={styles.topTitle}>登录</div>
-                    <div className={styles.topContent}>攀枝花市二十五中小教育集团业务系统</div>
+                    <div className={styles.topContent}>攀枝花市第二十五中小学校教育集团管理系统</div>
                 </div>
                 <div className={styles.content}>
                     <Spin spinning={loading}>
